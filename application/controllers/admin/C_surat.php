08d1115faf82cfff73c61a41cd326b6a7eeea682
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-10-20 15:41:43 +0700

    update surat

--- application/controllers/admin/C_surat.php
+++ application/controllers/admin/C_surat.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Janji extends CI_Controller {
+class C_surat extends CI_Controller {
 
     function __construct(){
         parent::__construct();	
