99ed6f94747c4ad077951792b47f51a94e66b325
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-10-20 17:16:07 +0700

    update surat

--- application/controllers/admin/C_surat.php
+++ application/controllers/admin/C_surat.php
@@ -48,3 +48,25 @@ class C_surat extends CI_Controller {
               // kode yang berfungsi mengarahkan pengguna ke link base_url()crud/index/ 
               redirect('admin/C_surat/index');
               }
+
+              function hapus($id){
+                // baaris kode ini berisi fungsi untuk menyimpan id user kedalam array $where pada index array bernama 'id'
+              $where = array('id_surat' => $id);
+              // kode di bawah ini untuk menjalankan query hapus yang berasal dari method hapus_data() pada model m_data
+                  $this->m_data_surat->hapus_data2($where,'admin');
+              // kode yang berfungsi mengarakan pengguna ke link base_url()crud/index/
+              redirect('admin/C_surat/index');
+              }
+
+              function edit($id){
+                // kode yang berfungsi untuk menyimpan id user ke dalam array $where pada index array benama id
+                $where = array('id_surat' => $id);
+                // kode di bawah ini adalah kode yang mengambil data user berdasarkan id dan disimpan kedalam array $data dengan index bernama user
+                $data['admin'] = $this->m_data_surat->edit_data($where,'admin')->result();
+                // kode ini memuat vie edit dan membawa data hasil query diatas
+                $this->load->view('admin/templates/header');
+                $this->load->view('admin/templates/sidebar');
+                $this->load->view('admin/v_editsurat',$data);
+                $this->load->view('admin/templates/footer');
+               
+            }
