06733d92eda72c396f668439560a364c5306fa4d
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-10-20 14:38:37 +0700

    update surat

---
+++ application/controllers/admin/C_surat.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Janji extends CI_Controller {
+
+    function __construct(){
+        parent::__construct();	
+            // ini adalah function untuk memuat model bernama m_data
+        $this->load->model('m_data_surat');
+        // 
+        $this->load->helper('url');
+
+            if($this->session->userdata('status') != "login") {
+                redirect(base_url("admin/C_login"));
+            }
+        }
+        //  method yang akan diakses saat controller ini diakses
+        public function tampil_surat(){
+            // ini adalah variabel array $data yang memiliki index user, berguna untuk menyimpan data 
+            $data['surat'] = $this->m_data_surat->tampil_surat()->result();
+            // ini adalah baris kode yang berfungsi menampilkan v_tampil dan membawa data dari tabel user
+            $this->load->view('admin/templates/header');
+            $this->load->view('admin/templates/sidebar');
+            $this->load->view('admin/v_surat', $data);
+            $this->load->view('admin/templates/footer');
+            }
+
+            
--- application/views/admin/v_surat.php
+++ application/views/admin/v_surat.php
@@ -8,3 +8,7 @@
   <div class="card-header py-3">
     <h6 class="m-0 font-weight-bold text-primary">Data Surat</h6>
   </div>
+  <div class="card-content collapse show">
+      <div class="card-body">
+          <a href="<?= base_url() . 'admin/c_surat/surat'; ?>"><button type="button" class="btn btn-primary btn-min-width mr-1 mb-1"><i class="ft-plus"> </i> Tambah Divisi</button></a>
+      </div>
