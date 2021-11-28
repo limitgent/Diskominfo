4bf4fd2c4b84a11f659e2505b1b025e1bbbce65d
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-11-28 21:07:26 +0700

    Update m_data_surat.php

--- application/models/m_data_surat.php
+++ application/models/m_data_surat.php
@@ -6,32 +6,34 @@ class m_data_surat extends CI_Model{
 		return $this->db->get('surat');
     }
 
-    function tambah_surat($data,$table){
+    function tambah_surat($surat,$table){
       $this->db->insert($table,$data);
       }
 
-      function hapus_surat($where,$table){
+    function hapus_surat($where,$table){
         $this->db->where($where);
         $this->db->delete($table);
-    }
+      }
 
     function edit_surat($where,$table){		
+        return $this->db->get_where($table,$where);
+      }
+    function tampil_surat_akhir()
+    {
+        $this->db->order_by('id_surat', 'DESC');
+        return $this->db->get('surat', 1);
+    }
+    function update_data_surat($where, $surat, $table)
+    {
+        $this->db->where($where);
+        $this->db->update($table, $surat);
+    }
+    function edit($where,$table){		
       return $this->db->get_where($table,$where);
-  }
-  
-  function tampil_surat_akhir()
-  {
-  $this->db->order_by('id_surat', 'DESC');
-  return $this->db->get('surat', 1);
-  }
-  
-
-function edit($where,$table){		
-  return $this->db->get_where($table,$where);
-}
+    }
 // method untuk mengupdate data ke dalam database 
-  function update($where,$data,$table){
+  function update($where,$surat,$table){
   $this->db->where($where);
-  $this->db->update($table,$data);
+  $this->db->update($table,$surat);
 }	
 }
