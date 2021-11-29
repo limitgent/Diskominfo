df94fa6a48400dd695c15e280ccaea108c518688
emilia <emilialaily12@gmail.com>
2021-11-29 09:52:47 +0700

    edit tampilan user

--- application/views/user/v_user.php
+++ application/views/user/v_user.php
@@ -1,20 +1,17 @@
+<!-- ***** Main Banner Area Start ***** -->
+<section class="section main-banner" id="top" data-section="section1">
+  <video autoplay muted loop id="bg-video">
+    <source src="<?php echo base_url(); ?>assets/user/images/video-slide.mp4" type="video/mp4" />
+  </video>
 
-  <!-- ***** Main Banner Area Start ***** -->
-  <section class="section main-banner" id="top" data-section="section1">
-      <video autoplay muted loop id="bg-video">
-          <source src="<?php echo base_url();?>assets/user/images/course-video.mp4" type="video/mp4" />
-      </video>
+  <div class="video-overlay header-text">
+    <div class="container">
+      <div class="row">
 
-      <div class="video-overlay header-text">
-          <div class="container">
-            <div class="row">
-              
-          </div>
-              </div>
-            </div>
-          </div>
       </div>
-  </section>
-  <!-- ***** Main Banner Area End ***** -->
-
- 
+    </div>
+  </div>
+  </div>
+  </div>
+</section>
+<!-- ***** Main Banner Area End ***** -->
