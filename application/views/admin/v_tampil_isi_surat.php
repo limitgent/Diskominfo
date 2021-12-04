fb3df5f4ee9276f4565eea806681e1fd87c298a4
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-04 17:09:51 +0700

    Update v_tampil_isi_surat.php

--- application/views/admin/v_tampil_isi_surat.php
+++ application/views/admin/v_tampil_isi_surat.php
@@ -1,32 +1,12 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<!-- Begin Page Content -->
-<div class="container-fluid">
-
-<!-- Page Heading -->
-
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Setting Karyawan Divisi</h6>
-  </div>
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-            <?php
-            $file = //nama folder
+   <?php
+            $surat = //surat
             $filename = $su->file;
             header ('Content-type: application/pdf');
             header ('Content-Disposition: inline; filename="'. $filename . '"');
             header ('Content-Transfer-Encoding: binary');
             header ('Accept-Ranges: bytes');
             @readfile($file);
-            ?>
+    ?>
