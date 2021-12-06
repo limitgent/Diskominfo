971a93751c69cf67d592d0977974142ec1d97425
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-06 21:44:02 +0700

    Update v_tampil_isi_surat.php

--- application/views/admin/v_tampil_isi_surat.php
+++ application/views/admin/v_tampil_isi_surat.php
@@ -1,12 +1,36 @@
 <!doctype html>
 <html lang="en">
   <head>
-   <?php
-            $surat = //surat
-            $filename = $su->file;
-            header ('Content-type: application/pdf');
-            header ('Content-Disposition: inline; filename="'. $filename . '"');
-            header ('Content-Transfer-Encoding: binary');
-            header ('Accept-Ranges: bytes');
-            @readfile($file);
-    ?>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+
+<!-- DataTales Example -->
+<div class="row">
+  <div class="col-sm-12">
+    <table class="table table-bordered">
+      <tr>
+        <th width="100px">ID Surat</th>
+        <th width="30px">:</th>
+        <td><?= $surat['id_surat'] ?></td>
+      </tr>
+      <tr>
+        <th width="100px">ID OPD</th>
+        <th width="30px">:</th>
+        <td><?= $surat['id_opd'] ?></td>
+      </tr>
+      <tr>
+        <th width="100px">Perihal</th>
+        <th width="30px">:</th>
+        <td><?= $surat['perihal'] ?></td>
+      </tr>
+</table>
+</div>
+<div class="col-sm-12"></div>
+  <iframe src="<?= base_url('surat/'.$surat['surat'])?>" style="border:2px solid blue;" height="600px" width="100%" title="iframe example"></iframe>
+</div>
