e4706f7633ed32feb6b1618e266b06e7c7dcf937
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-07 00:26:25 +0700

    Update v_dashboard.php

--- application/views/surat/v_dashboard.php
+++ application/views/surat/v_dashboard.php
@@ -3,6 +3,6 @@
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-  <h1 class="h3 mb-0 text-gray-800">surat</h1>
+  <h1 class="h3 mb-0 text-gray-800">Silahkan Input Data dengan Benar</h1>
 </div>
 </div>
