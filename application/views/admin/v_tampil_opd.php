6d548b00ef4aacd6094a96ba0ec1974642d57952
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-07 00:26:20 +0700

    Create v_tampil_opd.php

---
+++ application/views/admin/v_tampil_opd.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <div class="container-fluid">
+
+    <div class="card shadow mb-4">
+        
+        <div class="card-content collapse show">
+            <div class="card-body">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Setting Kehadiran
+            </button>
+            <div>
+            <div class="card-header py-3">
+              <h10 class="m-0 font-weight-bold text-primary">Data Karyawan Di Kantor</h10>
+            </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                      <thead>
+                        <tr>
+                          <th>Divisi</th>
+                          <th>Foto</th>
+                          <th>NIP</th>
+                          <th>Nama</th>
+                          <th>Jabatan</th>
+                          <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody> 
+                      <?php 
+                        foreach ($karyawan as $kar ) { ?>
+                          <tr>
+                          <td><?=$kar->id_divisi?></td>
+                            <td><?php
+                            if($kar->foto==''){?>
+                              <label>Belum Ada Gambar</label><br>
+                                <?php }else{ ?>
+                                  <img src="<?php echo base_url('assets/img/karyawan/'.$kar->foto)?>" width="90" height="110"><br>
+                                  <?php }?></td></td>
+                            <td><?=$kar->nip?></td>
+                            <td><?=$kar->nama_karyawan?></td>
+                            <td><?=$kar->jabatan?></td>
+                            <td><?=$kar->status?></td>
+                            <?php } ?>
+                        </tbody>
+                      </table>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Setting Kehadiran</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="<?php echo base_url() . 'admin/Janji/set_status/'; ?>" method="get">
+      <div class="modal-body">
+        <h5 class="mt-2">Pilih Divisi Karyawan</h5>
+        <fieldset class="form-group">
+        <select name="id_divisi" id="id_divisi" class\="custom-select">
+                                            
+            <?php
+                foreach ($set as $s) :
+            ?>
+                <option value="<?= $s->id_divisi;?>"><?= $s->nama_divisi;?></option>
+            <?php
+                endforeach;
+            ?>             
+        </select>
+        </fieldset>
+
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary">Cek</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </head>
+  <body>
+   
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
