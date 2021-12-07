20caff115c7ee5c19382e98f0731ae2e1e1ef91e
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-07 21:37:53 +0700

    Update v_tambahsurat.php

--- application/views/admin/v_tambahsurat.php
+++ application/views/admin/v_tambahsurat.php
@@ -9,7 +9,7 @@
         <!-- Content Row -->
     </div>
     <div class="col-lg-10">
-        <form action="<?php echo base_url() . 'admin/C_surat/aksi_tambah_surat'; ?>" method="post">
+        <?php echo form_open_multipart('admin/C_surat/aksi_tambah_surat') ;?>
 
         <div class="form-group">
                 <label for="id_surat"> ID Surat : </label>
@@ -32,7 +32,7 @@
                 <input type="text" class="form-control form-control-user" id="perihal" name="perihal" placeholder="masukkan perihal surat" title="masukkan perihal surat dengan benar" required>
             </div>
             <div class="form-group">
-                <label for="varchar"> File : </label>
+                <label for="file"> File : </label>
                 <input type="file" class="form-control form-control-user" id="file" name="file" placeholder="masukkan file surat" title="masukkan soft file surat" required>
             </div>
 
@@ -41,6 +41,7 @@
                 <button class="btn btn-primary px-2 mr-1" type="submit">Simpan</button>
                 <button class="btn btn-secondary" onclick="window.history.back()"><i class="fas fa-arrow-left"></i></button>
             </div>
+            <?php echo form_close();?>
             </form>
         <br>
         <div class="text-center">
