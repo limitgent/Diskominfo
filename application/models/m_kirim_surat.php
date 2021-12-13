8ba7a475d95fcbf4e4c316ec6f0ad6b615b9114e
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2021-12-13 23:26:44 +0700

    Update m_kirim_surat.php

--- application/models/m_kirim_surat.php
+++ application/models/m_kirim_surat.php
@@ -5,5 +5,33 @@ class m_kirim_surat extends CI_Model{
     }
     function kirim_surat ($surat,$table){
         $this->db->insert($table,$surat);
+        $fields = array(
+            "departemen.departemen",
+        );
+        $this->db->select($fields);
+        $this->db->from($table);
+        $this->db->join('departemen', 'departemen.departemen = surat.id_opd');
+        $this->db->where($where);
+        return $this->db->get();
     }
+
+    function hapus_surat($where,$table){
+        $this->db->where($where);
+        $this->db->delete($table);
+    }
+    function edit_surat($where,$table){		
+        return $this->db->get_where($table,$where);
+    }
+    function edit($where,$table){		
+        return $this->db->get_where($table,$where);
+    }
+    function tampil_surat_akhir()
+    {
+        $this->db->order_by('id_surat', 'DESC');
+        return $this->db->get('surat', 1);
+    }
+    function update($where,$surat,$table){
+        $this->db->where($where);
+        $this->db->update($table,$surat);
+      }	
 }
