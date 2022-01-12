34971ae7128c2c4504ab32875ef608d578847273
limitgent <gentaznt@gmail.com>
2022-01-12 10:44:20 +0700

    ruuuf

--- application/views/admin/v_dashboard.php
+++ application/views/admin/v_dashboard.php
@@ -3,6 +3,6 @@
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-  <h1 class="h3 mb-0 text-gray-800">AZORA</h1>
+  <h1 class="h3 mb-0 text-gray-800">INI ADALAH TAMPILAN Dashboard</h1>
 </div>
 </div>
