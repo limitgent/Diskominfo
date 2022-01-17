d8b9f9abed1310e7d7c19670caeb75000db81e36
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2022-01-17 22:09:36 +0700

    Update v_kirim_surat.php

--- application/views/surat/v_kirim_surat.php
+++ application/views/surat/v_kirim_surat.php
@@ -5,6 +5,7 @@
   <div class="card-header py-3">
     <h6 class="m-0 font-weight-bold text-primary">Data Surat</h6>
   </div>
+  <?php echo $this->session->flashdata('pesan'); ?>
   <div class="card-body">
   <div class="table-responsive">
   <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
