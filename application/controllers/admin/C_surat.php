f3c97faa734f5e2e70c28e5f3210f654779edf09
CiciliaShelin <55520105+ciciliaShelin@users.noreply.github.com>
2022-01-17 20:37:43 +0700

    Update C_surat.php

--- application/controllers/admin/C_surat.php
+++ application/controllers/admin/C_surat.php
@@ -120,6 +120,14 @@ class C_surat extends CI_Controller {
               // kode di bawah ini untuk menjalankan query hapus yang berasal dari method hapus_data() pada model m_data
                   $this->m_data_surat->hapus_surat($where,'surat');
               // kode yang berfungsi mengarakan pengguna ke link base_url()crud/index/
+              $this->session->set_flashdata('pesan', '
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                Anda <strong>berhasil</strong> menghapus data.
+                <button type="button" class="close py-auto" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              ');
               redirect('admin/C_surat/tampil_surat');
               }
 
